7a728115fc7155a91313e8c11259a6c3c35e431b
sanjaysingh24 <sanjubora84@gmail.com>
2026-04-19 20:09:38 +0530

    pdate

--- src/loaders/csv_loader.py
+++ src/loaders/csv_loader.py
@@ -8,14 +8,17 @@ def load_csv(file_path:str):
     try:
         loader = CSVLoader(file_path)
         docs=loader.load()
+        
+    
         for doc in docs:
             doc.page_content = clean_text(doc.page_content)
         splitter = RecursiveCharacterTextSplitter(
             chunk_size=1000,
-            chunk_overlap=100,
+            chunk_overlap=10,
             separators=["\n\n","\n","."," ", ""]
         )
         chunks = splitter.split_documents(docs)
+
         vectordb = store_in_pinecoce(chunks)
         if vectordb["status"]==True:
             fdelete = delete_file(file_path)
